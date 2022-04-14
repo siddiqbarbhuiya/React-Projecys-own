aa689c169958e1787be405126c2bb371c8e646d9
siddiqbarbhuiya <siddiqbarbhuiya@outlook.com>
2022-04-15 00:35:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,62 @@
-import './App.css';
-import FetchNews from './FetchNews';
+import "./App.css";
 
-function App() {
+import React, {useRef, useState, useEffect} from "react";
+import './App.css';
+
+const App =() => {
+  const [result, setResult] = useState("")
+  const inputRef = useRef(null);
+
+  useEffect(() => inputRef.current.focus())
+
+  const handleClick = (e) => {
+    setResult(result.concat(e.target.name));
+    if(result === "Error"){
+      setResult("")
+    }
+  }
+
+  const backSpace = () => {
+    setResult(result.slice(0, -1))
+  }
+
+  const clear = () => setResult("");
+
+  const calculate = () => {
+    try{
+      setResult(eval(result).toString())
+      // setResult(eval(result) + "")
+    } catch {
+      setResult("Error")
+    }
+  }
+
   return (
-    <FetchNews />
+    <div className="calc-app">
+      <form>
+        <input type="text" value={result} ref={inputRef} />
+      </form>
+      <div className="keypad">
+        <button onClick={clear} id="clear">Clear</button>
+        <button onClick={backSpace} id="backspace">C</button>
+        <button name="+" onClick={handleClick}>+</button>
+        <button name="7" onClick={handleClick}>7</button>
+        <button name="8" onClick={handleClick}>8</button>
+        <button name="9" onClick={handleClick}>9</button>
+        <button name="-" onClick={handleClick}>-</button>
+        <button name="4" onClick={handleClick}>4</button>
+        <button name="5" onClick={handleClick}>5</button>
+        <button name="6" onClick={handleClick}>6</button>
+        <button name="*" onClick={handleClick}>*</button>
+        <button name="1" onClick={handleClick}>1</button>
+        <button name="2" onClick={handleClick}>2</button>
+        <button name="3" onClick={handleClick}>3</button>
+        <button name="/" onClick={handleClick}>/</button>
+        <button name="0" onClick={handleClick}>0</button>
+        <button name="." onClick={handleClick}>.</button>
+        <button onClick={calculate} id="result">Result</button>
+      </div>
+    </div>
   );
 }
 
