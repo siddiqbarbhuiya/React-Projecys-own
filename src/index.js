768779ade3fb00ada5c27159b4439c8af71be1f4
siddiqbarbhuiya <siddiqbarbhuiya@outlook.com>
2022-05-27 00:09:35 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import Context from './Context'
-
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import fontawesome from "./fontawesome";
+import App from "./App";
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
-    <Context>
+  <StrictMode>
     <App />
-    </Context>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </StrictMode>,
+  rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
